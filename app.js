b9975977b6347871ee2aedf4be95dcd59d2e0724
Arslan <muhammadarslanrajput12@gmail.com>
2024-03-24 17:33:59 -0700

    javascript

---
+++ app.js
@@ -0,0 +1,48 @@
+// let NameOfItem1=prompt("Type Name Of item 1")
+// let priceOfItem1=+prompt("price Of Item 1")
+// let orderQuntityItem1=+prompt("Order Quntity Item 1")
+// let NameOfItem2=prompt("Type Name Of item 2")
+// let priceOfItem2=+prompt("price Of Item 2")
+// let orderQuntityItem2=+prompt("Order Quntity Item 2")
+// let ShopingCharges=+prompt("Shoping Charges")
+// let totel=((priceOfItem1+priceOfItem2+orderQuntityItem1+orderQuntityItem2+ShopingCharges))
+// document.write("Name of item1"+NameOfItem1+"<br />")
+// document.write("Name of item2"+NameOfItem2+"<br />")
+// document.write("price Of Item 1"+priceOfItem1+"<br />")
+// document.write("price Of Item 2"+priceOfItem2+"<br />")
+// document.write("Order Quntity Item 1"+orderQuntityItem1+"<br />")
+// document.write("Order Quntity Item 2"+orderQuntityItem2+"<br />")
+// document.write("Shoping Charges"+ShopingCharges+"<br />")
+// document.write("Totel bill"+totel+"<br />")
+// if (totel>=2000){
+//     document.write("Offer them 10% discount ")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// let user=+prompt("type number")
+// if(user%2){
+//     document.write("This is Odd Number")
+// }
+// else( document.write("This is Even Number"))
+
+
+
+let arry=prompt("type  day")
+let weak="Monday,tuesday,thusday,friday"
+if(arry.toLowerCase()==="monday".toLowerCase()||
+  arry.toLowerCase()==="tuesday".toLowerCase()||
+arry.toLowerCase()==="thursday".toLowerCase() ||
+arry.toLowerCase()==="friday".toLowerCase() ){
+    document.write("Its a week day”.");
+}
+else(document.write("Yay! Its a holiday."))
